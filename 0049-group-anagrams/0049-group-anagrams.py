9497d37c2c7e0ef2611c92ba708d0822a9607490
Rakesh Doddamani <64364295+RakeshIIT@users.noreply.github.com>
2024-02-12 09:36:04 +0530

    Time: 94 ms (44.15%), Space: 22 MB (23.72%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ref=defaultdict(list)
-        for w in strs:
+        ref = {}
+        for word in strs:
             li=[0]*26
-            for c in w:
+            for c in word:
                 li[ord(c)-ord('a')]+=1
-            ref[tuple(li)].append(w)
+            if tuple(li) in ref:
+                ref[tuple(li)].append(word)
+            else:
+                ref[tuple(li)] = [word]
         return ref.values()
